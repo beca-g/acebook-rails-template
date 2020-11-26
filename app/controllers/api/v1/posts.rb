c92b778127f8501db791a219fb0c98670a8d6815
Meryl Rastall <merylrastall@gmail.com>
2020-11-26 16:27:56 +0000

    updated post.rb file to correct the update route

--- app/controllers/api/v1/posts.rb
+++ app/controllers/api/v1/posts.rb
@@ -4,58 +4,46 @@ module API
       include API::V1::Defaults
       resource :posts do
 
-        # helpers do
-        #   def current_user
-        #     @current_user ||= User.authorize!(env)
-        #   end
-        # end
-
-
-      desc "Creates a new post"
+        desc "Creates a new post"
         params do 
           requires :message, type: String, desc: "message of the post"
           requires :user_id, type: String, desc: "Username of current user"
         end 
         post do
+          p params
           Post.create!(declared(params))
         end 
 
-      desc "Return all posts"
+        desc "Return all posts"
         get "" do
           Post.all
         end
 
-      desc "Return a post"
+        desc "Return a post"
         params do
-          requires :id, type: String, desc: "ID of the post"
+            requires :id, type: String, desc: "ID of the post"
         end
         get ":id" do
           Post.where(id: permitted_params[:id]).first!
         end
 
-      # currently not working
-      # desc "amends a post"
-      #   params do
-      #     requires :message, type: String, desc: "updated message"
-      #     requires :user_id, type: String, desc: "ID of the user"
-      #   end
-      #   route_param :id do
-      #   put ':id' do
-      #   Post.find(params[:id]).update
-   
-      #   end
-      # end
-
-
-      desc "deletes a post"
+        desc "amends a post"
         params do
-        requires :id, type: String
-      end
-        delete ':id' do
-        Post.find(params[:id]).destroy!
-      end
-     
+          requires :message, type: String, desc: "updated message"
+          requires :id, type: String, desc: "id of the post"
+        end
+        put ':id' do
+          Post.find(params[:id]).update(declared(params))
+        end
 
+
+        desc "deletes a post"
+        params do
+          requires :id, type: String
+        end
+        delete ':id' do
+          Post.find(params[:id]).destroy!
+        end
 
       end
     end
